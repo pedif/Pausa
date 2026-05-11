7bc0206f3b8a35e5b60956331c8b57ff99afe24d
Pedif <foroughipedram@gmail.com>
2026-05-11 16:43:09 +0330

    add end of timer screen
    Eye timer and quick timers now have an independant screen to be displayed when they are done

---
+++ feature/eye/src/main/java/com/techys/pausa/eye/component/EyeEndScreen.kt
@@ -0,0 +1,82 @@
+package com.techys.pausa.eye.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.techys.core.model.TimerState
+import com.techys.core.service.PausaService
+import com.techys.core.util.TimeUtil
+import com.techys.designsystem.theme.AppTheme
+import com.techys.designsystem.theme.Dimen
+import kotlinx.coroutines.delay
+
+@Composable
+fun EyeEndScreen(
+    modifier: Modifier = Modifier,
+    onFinished: () -> Unit = {}
+) {
+    val state by PausaService.state.collectAsState()
+    var remainingTime by remember { mutableIntStateOf(5) }
+
+    EyeEndScreen(
+        state = state.quickTimers.first(),
+        seconds = remainingTime,
+        modifier = modifier
+    )
+
+    LaunchedEffect(Unit) {
+        while (remainingTime > 0) {
+            delay(1_000)
+            remainingTime--
+        }
+        onFinished()
+    }
+}
+
+@Composable
+fun EyeEndScreen(
+    state: TimerState,
+    seconds: Int,
+    modifier: Modifier = Modifier
+) {
+
+    Column(
+        modifier = modifier.fillMaxSize().background(MaterialTheme.colorScheme.background).padding(Dimen.paddingScreenHorizontal),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("${state.title} has just finished",
+            style = MaterialTheme.typography.headlineLarge,
+            color = MaterialTheme.colorScheme.primary)
+        Text("Automatically dismissing this screen in" +
+                " ${TimeUtil.getElapsedTimeLabel(seconds)}",
+            style = MaterialTheme.typography.titleMedium,
+            color = MaterialTheme.colorScheme.primary)
+    }
+
+}
+
+@Preview
+@Composable
+private fun PreviewScreen() {
+    AppTheme {
+        EyeEndScreen(
+            state = TimerState(id = 1),
+            seconds = 10
+        )
+    }
+}
