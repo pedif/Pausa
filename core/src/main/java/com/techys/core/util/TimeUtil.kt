168401fa400c22d2ba47626fcf33bbed2ddd2970
Pedif <foroughipedram@gmail.com>
2026-06-03 09:51:39 +0330

    removed localization from timer elapsed time

--- core/src/main/java/com/techys/core/util/TimeUtil.kt
+++ core/src/main/java/com/techys/core/util/TimeUtil.kt
@@ -1,9 +1,18 @@
 package com.techys.core.util
 
 import android.text.format.DateUtils
+import kotlin.math.min
 
 object TimeUtil {
     fun getElapsedTimeLabel(timeInSeconds: Int): String {
-        return DateUtils.formatElapsedTime(timeInSeconds.toLong())
+        val hours = timeInSeconds / (60 * 60)
+        val minutes = (timeInSeconds % (60 * 60)) / 60
+        val seconds = timeInSeconds % 60
+        return if (hours > 0) {
+         "${pad2(hours)}:${pad2(minutes)}:${pad2(seconds)}"
+        } else {
+            "${pad2(minutes)}:${pad2(seconds)}"
+        }
     }
+    private fun pad2(value: Int): String = value.toString().padStart(2, '0')
 }
