d5e040c7bece5c12b5068d7f410d50189e0b2b66
Pedif <foroughipedram@gmail.com>
2026-06-14 12:12:39 +0330

    add full screen intent permission

--- core/src/main/java/com/techys/core/permission/PermissionUtil.kt
+++ core/src/main/java/com/techys/core/permission/PermissionUtil.kt
@@ -2,6 +2,7 @@ package com.techys.core.permission
 
 import android.Manifest
 import android.app.AlarmManager
+import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -10,6 +11,7 @@ import android.os.Build
 import android.os.PowerManager
 import android.provider.Settings
 import androidx.core.app.ActivityCompat
+import androidx.core.net.toUri
 import dagger.hilt.android.qualifiers.ApplicationContext
 import java.util.jar.Attributes
 import javax.inject.Inject
@@ -30,13 +32,21 @@ object PermissionUtil {
 
     fun hasAlarmPermission(
         context: Context
-    ): Boolean{
+    ): Boolean {
         val alarmMgr = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             return alarmMgr.canScheduleExactAlarms()
         }
         return true
+    }
+
+    fun hasFullScreenIntentPermission(context: Context): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            val nm = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            nm.canUseFullScreenIntent()
+        } else
+            true
     }
 
 
@@ -65,5 +75,15 @@ object PermissionUtil {
     fun openBatterySettings(context: Context) {
         val intent = Intent(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS)
         context.startActivity(intent)
+    }
+
+    fun openFullScreenIntentSettings(context: Context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            val intent = Intent(
+                Settings.ACTION_MANAGE_APP_USE_FULL_SCREEN_INTENT,
+                "package:${context.packageName}".toUri()
+            )
+            context.startActivity(intent)
+        }
     }
 }
